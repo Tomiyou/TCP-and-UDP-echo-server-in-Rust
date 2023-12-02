e33f5eab3ab558ac865189c519a2f3489363249d
Tomaž Hribernik <tomaz.hribernik@protonmail.com>
2023-12-02 23:42:52 +0100

    Validate UDP bind port and peer address

--- src/udp_client.rs
+++ src/udp_client.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use std::io::{stdin, stdout, Write};
-use std::net::UdpSocket;
+use std::net::{UdpSocket, SocketAddr};
 
 #[derive(Parser)]
 struct Arguments {
@@ -8,24 +8,28 @@ struct Arguments {
     bind_port: String,
 
     /// Address of the other client [address:port]
-    host_address: String,
+    peer_address: String,
 }
 
 fn main() -> std::io::Result<()> {
-    // Parse server address from CLI arguments
-    let stdin = stdin();
-    let mut stdout = stdout();
-
     // Parse arguments
     let args = Arguments::parse();
 
+    let stdin = stdin();
+    let mut stdout = stdout();
+
+    // Check bind port is valid
+    args.bind_port.parse::<u16>().expect("Bad bind port given");
+
+    // Check peer address is valid
+    let peer_address: SocketAddr = args.peer_address
+        .parse()
+        .expect("Bad peer address given, expected \"ip_addres:port\"");
+
+    // Bind to local address:port
     let mut bind_address = "[::]:".to_string();
     bind_address.push_str(&args.bind_port);
-    println!("Bind port: {}", bind_address);
     let socket = UdpSocket::bind(bind_address)?;
-
-    let host_address = args.host_address;
-    println!("Host address: {}", host_address);
 
     // Get user input
     let mut user_input = String::new();
@@ -35,7 +39,7 @@ fn main() -> std::io::Result<()> {
     let user_input = user_input.as_bytes();
 
     loop {
-        // Read user input
+        // Read how many times to send
         let mut send_count = String::new();
         print!("How many times to send: ");
         stdout.flush().unwrap();
@@ -45,9 +49,9 @@ fn main() -> std::io::Result<()> {
         let send_count: u32 = send_count.parse().unwrap_or(1);
         println!("Sending {} packets", send_count);
 
-        // Write TCP stream
+        // Send via UDP stream
         for _ in 0..send_count {
-            socket.send_to(user_input, &host_address).unwrap();
+            socket.send_to(user_input, &peer_address).unwrap();
         }
     }
 }
