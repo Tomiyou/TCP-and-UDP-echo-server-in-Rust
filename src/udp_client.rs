dccb4ff969b4989055cc323e86c9fd4eb3779a9c
Tomaž Hribernik <tomaz.hribernik@protonmail.com>
2023-12-03 00:32:23 +0100

    Add default value to sent text via UDP

--- src/udp_client.rs
+++ src/udp_client.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin, stdout, Write, Stdin};
 use std::net::{UdpSocket, SocketAddr};
 use std::process::exit;
 use std::str::from_utf8;
@@ -35,10 +35,7 @@ fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind(bind_address)?;
 
     // Get user input
-    let mut user_input = String::new();
-    print!("Input the text to send: ");
-    stdout.flush().unwrap();
-    stdin.read_line(&mut user_input).unwrap();
+    let user_input = get_user_text(&stdin, args.bind_port);
     let user_input = user_input.as_bytes();
 
     let recv_socket = socket.try_clone().unwrap();
@@ -59,7 +56,7 @@ fn main() -> std::io::Result<()> {
     loop {
         // Read how many times to send
         let mut send_count = String::new();
-        print!("Enter how many packets to send: ");
+        print!("Enter how many packets to send (default 1): ");
         stdout.flush().unwrap();
 
         stdin.read_line(&mut send_count).unwrap();
@@ -71,5 +68,26 @@ fn main() -> std::io::Result<()> {
         for _ in 0..send_count {
             socket.send_to(user_input, &peer_address).unwrap();
         }
+    }
+}
+
+fn get_user_text(stdin: &Stdin, bind_port: String) -> String {
+    let mut stdout = stdout();
+    let mut user_text = String::new();
+    let mut default_text = "socket ".to_string();
+    default_text.push_str(&bind_port);
+
+    print!("Input some text to send (default \"{}\"): ", default_text);
+    stdout.flush().unwrap();
+
+    // Read a max 1024 character long string
+    stdin.read_line(&mut user_text).unwrap();
+    let mut user_text = user_text.replace('\n', "");
+    user_text.truncate(100);
+
+    if user_text.is_empty() {
+        default_text.to_string()
+    } else {
+        user_text
     }
 }
